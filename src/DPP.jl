7bb0108f5fba873eb666b41ebfcd331539522967
Simon Barthelme <simon.barthelme@grenoble-inp.fr>
2019-11-27 13:17:50 +0100

    basic subset kernels, can now index ensembles like matrices

--- src/DPP.jl
+++ src/DPP.jl
@@ -1,6 +1,6 @@
 module DPP
 using LinearAlgebra,StatsBase,Combinatorics,Distances,MLKernels
-import Base.show
+import Base.show,Base.getindex
 export sample,marginal_kernel,gaussker,rescale!,FullRankEnsemble,LowRankEnsemble,ProjectionEnsemble, polyfeatures, rff, inclusion_prob, cardinal
 
 include("lensemble.jl")
@@ -8,6 +8,6 @@ include("kdpp.jl")
 include("saddlepoint.jl")
 include("features.jl")
 include("sampling.jl")
-
+include("subset_kernels.jl")
 end # module
 
